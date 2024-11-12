49796119a315f21007ca0c6fe688a71f39348819
JhnRavelo <johnravelo135@gmail.com>
2024-11-12 16:34:59 +0300

    init project

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const db = require("./database/models");
+require("dotenv").config();
+const app = express();
+const mysql = require("mysql2/promise");
+
+mysql
+  .createConnection({
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+  })
+  .then((connection) => {
+    connection
+      .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE_NAME};`)
+      .then(() => {
+        db.sequelize.options.logging = false;
+        db.sequelize.sync({ alter: true }).then(() => {
+          app.listen(process.env.SERVER_PORT, async () => {
+            try {
+              console.log(`http://localhost:${process.env.SERVER_PORT}`);
+            } catch (error) {
+              console.log("BUILD", error);
+            }
+          });
+        });
+      });
+  });
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.static("public/"));
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+const stockRouter = require("./routers/stockRouter");
+app.use("/stock", stockRouter);
